5d1848a29373310c38fd3976a97b503c38c1b2ef
PawelNiedzwiecki <pawel.niedzwiecki@sky.uk>
2023-10-31 16:18:41 +0000

    init: initial commit

---
+++ src/components/service-map.tsx
@@ -0,0 +1,52 @@
+import { LatLngExpression } from 'leaflet';
+import React, { useEffect, useState } from 'react';
+import { MapContainer, Polygon, TileLayer } from 'react-leaflet';
+
+const ServiceMap = () => {
+  const mapCenter: LatLngExpression = [51.3018, -0.5603];
+  const latlngs: LatLngExpression[] = [
+    [-0.626843, 51.1741319],
+    [-0.3127429, 51.2181829],
+    [-0.3070834, 51.3048792],
+    [-0.3580132, 51.3740558],
+    [-0.4211846, 51.396339],
+    [-0.5138817, 51.4014798],
+    [-0.7953796, 51.3503932],
+    [-0.8481547, 51.2791339],
+    [-0.7987162, 51.2086394],
+  ];
+
+  const [coordinates, setCoordinates] = useState<LatLngExpression[]>([]);
+
+  useEffect(() => {
+    setCoordinates(latlngs.map((row) => [row[1], row[0]]));
+  }, []);
+
+  const purpleOptions = {
+    color: '#e11d48',
+    weight: 1,
+    opacity: 0.7,
+  };
+
+  return (
+    <>
+      <h3 className="mb-4 text-xl font-extrabold text-gray-900">
+        Working mobile in this area
+      </h3>
+      <MapContainer
+        style={{ height: '40vh', borderRadius: '10px' }}
+        center={mapCenter}
+        zoom={10}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution="&copy; Omniscale 2023 (https://maps.omniscale.com) – Map data: OpenStreetMap (License ODbL)"
+          url="https://maps.omniscale.net/v2/snowdrop-d2e964c4/style.grayscale/{z}/{x}/{y}.png"
+        />
+        <Polygon pathOptions={purpleOptions} positions={coordinates} />
+      </MapContainer>
+    </>
+  );
+};
+
+export default React.memo(ServiceMap);
